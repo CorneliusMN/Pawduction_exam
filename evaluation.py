9281743147a076da15d0c33767d2dfff9f315417
pietrorebecchi <pietrorebecchi003@gmail.com>
2025-11-12 11:34:19 +0100

    feat(eval): add functions for MLflow model stage transition and monitoring

--- evaluation.py
+++ evaluation.py
@@ -4,6 +4,7 @@ import json
 import mlflow
 from mlflow.tracking import MlflowClient
 import pandas as pd
+import time
 
 
 
@@ -185,3 +186,39 @@ def register_best_model(
         print("No run id is provided.")
         
         return None
+
+
+# Deploy section cell 86 notebook
+
+def wait_for_development(
+    model_name,
+    model_version,
+    stage = "Staging"):
+    client = MlflowClient()
+    status = False
+    while not status:
+        model_version_details = dict(client.get_model_version(name=model_name, version=model_version))
+        if model_version_details["current_stage"] == stage:
+            print(f"Transition completed to {stage}")
+            status = True
+            break
+        else:
+            time.sleep(2)
+    return status
+
+
+def run_stage_transition(
+    model_name,
+    model_version):
+    client = MlflowClient()
+    model_version_details = dict(client.get_model_version(name = model_name, version = model_version))
+    model_status = True
+    if model_version_details["current_stage"] != "Staging":
+        client.transition_model_version_stage(
+            name=model_name,
+            version=model_version, stage = "Staging",
+            archive_existing_versions=True)
+        model_status = wait_for_development(model_name, model_version, "Staging")
+    else:
+        print("Model already in staging")
+    return model_status
