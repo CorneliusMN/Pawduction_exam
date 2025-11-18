10aaf4679667e084e95fb3b06b19667b85debfde
szonjauley <szul@itu.dk>
2025-11-18 16:15:52 +0100

    feat(util): adding describe_numeric_col function

---
+++ source/util.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def describe_numeric_col(x): 
+    """
+    Calculates various descriptive stats for a numeric column in a dataframe.
+    Parameters:
+        x (pd.Series): Pandas col to describe.
+    Output:
+        y (pd.Series): Pandas series with descriptive stats. 
+    """
+    return pd.Series(
+        [x.count(), x.isnull().count(), x.mean(), x.min(), x.max()],
+        index=["Count", "Missing", "Mean", "Min", "Max"]
+    )
