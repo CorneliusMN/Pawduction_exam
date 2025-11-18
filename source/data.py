09f9a2d5f9f89440584ea793bd2a30810c95d133
szonjauley <szul@itu.dk>
2025-11-18 16:00:00 +0100

    feat(data): add argparse parameters for configurable min and max dates

--- source/data.py
+++ source/data.py
@@ -1,5 +1,8 @@
+import datetime
+
 import pandas as pd
 import subprocess
+import argparse
 
 from config import RAW_DATA_DIR
 
@@ -10,3 +13,20 @@ subprocess.run(["dvc", "pull"], check=True)
 print("Loading training data")
 data = pd.read_csv(RAW_DATA_DIR / "raw_data.csv")
 print("Total rows:", len(data)) #data.count()
+
+# Parameterize min and max date
+parser = argparse.ArgumentParser()
+parser.add_argument("--min_date", type=str, default=None)
+parser.add_argument("--max_date", type=str, default=None)
+args = parser.parse_args()
+
+min_date = (
+    pd.to_datetime(args.min_date).date() 
+    if args.min_date 
+    else pd.to_datetime("2024-01-01").date()
+)
+max_date = (
+    pd.to_datetime(args.max_date).date() 
+    if args.max_date 
+    else pd.to_datetime(datetime.datetime.now().date()).date()
+)
