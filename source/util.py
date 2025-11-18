2561cf9a4862b504434a4b7175f8fe689ba929b4
szonjauley <szul@itu.dk>
2025-11-18 16:17:09 +0100

    feat(util): adding impute_missing_values function

--- source/util.py
+++ source/util.py
@@ -12,3 +12,16 @@ def describe_numeric_col(x):
         [x.count(), x.isnull().count(), x.mean(), x.min(), x.max()],
         index=["Count", "Missing", "Mean", "Min", "Max"]
     )
+
+def impute_missing_values(x, method="mean"):
+    """
+    Imputes the mean/median for numeric columns or the mode for other types.
+    Parameters:
+        x (pd.Series): Pandas col to describe.
+        method (str): Values: "mean", "median"
+    """
+    if (x.dtype == "float64") | (x.dtype == "int64"):
+        x = x.fillna(x.mean()) if method=="mean" else x.fillna(x.median())
+    else:
+        x = x.fillna(x.mode()[0])
+    return x
