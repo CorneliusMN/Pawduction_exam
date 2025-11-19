7240ba59cac42644412e09d2764b2cdac447d0b7
szonjauley <szul@itu.dk>
2025-11-19 12:38:50 +0100

    feat(train): define MLFlow experiment based on current date

--- source/train.py
+++ source/train.py
@@ -1,5 +1,8 @@
+import datetime
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import mlflow
 
 from config import PROCESSED_DATA_DIR
 
@@ -14,3 +17,10 @@ X = data.drop(["lead_indicator"], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, random_state=42, test_size=0.15, stratify=y
 )
+
+# Define MLflow experiment
+current_date = datetime.datetime.now().strftime("%Y_%B_%d")
+experiment_name = current_date
+mlflow.set_experiment(experiment_name)
+experiment = mlflow.get_experiment_by_name(experiment_name)
+experiment_id = experiment.experiment_id
