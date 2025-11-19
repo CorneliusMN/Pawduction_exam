a69e82e9d9ceaa1aa67402068335bfaf2b942934
szonjauley <szul@itu.dk>
2025-11-19 12:40:57 +0100

    feat(train): add MLflow tracking and hyperparameter search for XGBoost model

--- source/train.py
+++ source/train.py
@@ -24,3 +24,32 @@ experiment_name = current_date
 mlflow.set_experiment(experiment_name)
 experiment = mlflow.get_experiment_by_name(experiment_name)
 experiment_id = experiment.experiment_id
+
+# Track XGBoost with MLflow
+with mlflow.start_run(experiment_id=experiment_id, run_name="xgboost_rf") as run:
+    
+    # Enable XGBoost autologging
+    mlflow.xgboost.autolog()
+    
+    model = XGBRFClassifier(random_state=42)
+    param_dist = {
+        "learning_rate": uniform(1e-2, 3e-1),
+        "min_split_loss": uniform(0, 10),
+        "max_depth": randint(3, 10),
+        "subsample": uniform(0, 1),
+        "objective": ["reg:squarederror", "binary:logistic", "reg:logistic"],
+        "eval_metric": ["aucpr", "error"]
+    }
+
+    # Hyperparameter search
+    grid_xgb = RandomizedSearchCV(model, param_distributions=param_dist, verbose=3, n_iter=10, cv=3)
+    grid_xgb.fit(X_train, y_train)
+    
+    best_xgb_model = grid_xgb.best_estimator_
+    
+    # Save the model locally
+    xgb_model_path = Path("./artifacts/lead_model_xgboost.json")
+    best_xgb_model.save_model(str(xgb_model_path))
+    
+    # Log data version from DVC
+    mlflow.log_param("data_version", dvc.api.get_url(PROCESSED_DATA_DIR / "dataset.csv"))
