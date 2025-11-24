200d38e1333a9b6fa485cc59a861dd1a0b8430a5
CorneliusMN <157996139+CorneliusMN@users.noreply.github.com>
2025-11-24 09:21:34 +0100

    feat(preprocess): add preprocessing file to main
    * init: initial commit adding preprocess file
    
    * feat(preprocess): adding basic_cleaning function
    
    * feat(preprocess): adding split_cat_conf function
    
    * feat(preprocess): adding handle_outliers function
    
    * feat(preprocess): adding impute function
    
    * feat(preprocess): adding scaler function
    
    * feat(preprocess): adding combine_and_document_drift function
    
    * feat(preprocess): adding binning function
    
    * feat(preprocess): adding preprocess function to call the other functions
    
    * chore(preprocess.py): remove unused diagnostic summary call
    
    * feat(preprocess): adding onehot_encode function
    
    * style(preprocess.py): changing import ordering to match pep8 format
    
    * docs(preprocess.py): adding docstrings for functions

---
+++ source/preprocess.py
@@ -0,0 +1,149 @@
+import json
+import joblib
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+from ..utils import impute_missing_values
+
+def describe_numeric_col(x: pd.Series) -> pd.Series:
+    '''
+    Parameters:
+        x (pd.Series): Pandas col to describe.
+    Output:
+        y (pd.Series): Pandas series with descriptive stats. 
+    '''
+    return pd.Series(
+        [x.count(), x.isnull().sum(), x.mean(), x.min(), x.max()],
+        index=["Count", "Missing", "Mean", "Min", "Max"],
+    )
+
+def basic_cleaning(df: pd.Dataframe) -> pd.Dataframe:
+    '''
+    Takes a pandas Dataframe and drop columns.
+    Then performs string cleaning
+    '''
+    df = df.copy()
+    #dropping columns
+    to_drop = ["is_active", "marketing_consent", "first_booking", "existing_customer", "last_seen", "domain", "country", "visited_learn_before_booking", "visited_faq"]
+    df = df.drop(columns = [c for c in to_drop])
+
+    #do string cleaning
+    for c in ["lead_indicator", "lead_id", "customer_code"]:
+        df[c].replace("", np.nan, inplace=True)
+    
+    df = df.dropna(subset=["lead_indicator", "lead_id"], axis = 0)
+    df = df[df.source == "signup"]
+    return df
+
+def split_cat_cont(df: pd.DataFrame):
+    '''
+    Splits a pandas dataframe into categorical and continuous
+    '''
+    vars = ["lead_id", "lead_indicator", "customer_group", "onboarding", "source", "customer_code"]
+    for col in vars:
+        df[col] = df[col].astype("object")
+    
+    cont_vars = df.loc[:, ((df.dtypes=="float64")|(df.dtypes=="int64"))]
+    cat_vars = df.loc[:, (df.dtypes=="object")]
+
+    return cat_vars, cont_vars
+
+def handle_outliers(cont_vars: pd.Dataframe) -> pd.DataFrame:
+    '''
+    Handles outliers for cont vars and prints summary to csv
+    '''
+    cont_vars_cleaned = cont_vars.apply(lambda x: x.clip(lower = (x.mean()-2*x.std()), upper = (x.mean()+2*x.std())))
+    outlier_summary = cont_vars_cleaned.apply(describe_numeric_col).T
+    outlier_summary.to_csv('./artifacts/outlier_summary.csv')
+    return cont_vars_cleaned
+
+def impute(cat_vars: pd.DataFrame, cont_vars: pd.Dataframe):
+    '''
+    impute missing values using utils function
+    '''
+    cat_missing_impute = cat_vars.mode(numeric_only=False, dropna=True)
+    cat_missing_impute.to_csv("./artifacts/cat_missing_impute.csv")
+
+    # Continuous variables missing values
+    cont_vars = cont_vars.apply(impute_missing_values)
+
+    # Cat vars missing values
+    cat_vars.loc[cat_vars['customer_code'].isna(),'customer_code'] = 'None'
+    cat_vars = cat_vars.apply(impute_missing_values)
+    cat_vars.apply(lambda x: pd.Series([x.count(), x.isnull().sum()], index = ['Count', 'Missing'])).T
+
+    return cat_vars, cont_vars
+
+def scale(cont_vars: pd.DataFrame, scaler_path):
+    '''
+    scale the cont vars
+    '''
+    scaler = MinMaxScaler()
+    scaler.fit(cont_vars)
+
+    joblib.dump(value=scaler, filename=scaler_path)
+    cont_vars = pd.DataFrame(scaler.transform(cont_vars), columns=cont_vars.columns)
+
+    return cont_vars
+
+def combine_and_document_drift(cont_vars: pd.DataFrame, cat_vars: pd.Dataframe) -> pd.Dataframe:
+    '''
+    combine the cont vars and cat vars then dump into json for data drift comp
+    '''
+    #combining the data
+    cont_vars = cont_vars.reset_index(drop=True)
+    cat_vars = cat_vars.reset_index(drop=True)
+    data = pd.concat([cat_vars, cont_vars], axis=1)
+
+    #exporting data drift artifact
+    data_columns = list(data.columns)
+    with open('./artifacts/columns_drift.json','w+') as f:           
+        json.dump(data_columns,f)
+    
+    data.to_csv('./artifacts/training_data.csv', index=False)
+
+    return data
+
+def binning(data: pd.DataFrame) -> pd.Dataframe:
+    '''
+    bin the data
+    '''
+    data = data.copy()
+    data['bin_source'] = data['source']
+    values_list = ['li', 'organic','signup','fb']
+    data.loc[~data['source'].isin(values_list),'bin_source'] = 'Others'
+    mapping = {'li' : 'socials', 
+            'fb' : 'socials', 
+            'organic': 'group1', 
+            'signup': 'group1'
+            }
+    data['bin_source'] = data['source'].map(mapping)
+    return data
+
+def preprocess_pipeline(df: pd.DataFrame,
+                        scaler_path: str = "./artifacts/minmax_scaler.joblib",
+                        save_gold_path: str = "./artifacts/train_data_gold.csv") -> pd.DataFrame:
+    '''
+    takes raw data and runs all above functions
+    returns a csv with the gold data
+    '''
+    data = df.copy()
+    #apply cleaning
+    data = basic_cleaning(data)
+    #apply splitting
+    cat_vars, cont_vars = split_cat_cont(data)
+    #handle outliers
+    cont_vars_cleaned = handle_outliers(cont_vars)
+    #impute missing
+    cat_vars_imputed, cont_vars_imputed = impute(cat_vars, cont_vars_cleaned)
+    #scale cont
+    cont_vars_scaled = scale(cont_vars_imputed, scaler_path=scaler_path)
+    #combine it together
+    combined = combine_and_document_drift(cont_vars_scaled, cat_vars_imputed)
+    #bin it
+    final = binning(combined)
+    #save gold data
+    final.to_csv(save_gold_path, index=False)
+    return final
