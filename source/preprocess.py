ab32e774dbcb33cf1861e74e818690d70d6dde9e
CorneliusMN <157996139+CorneliusMN@users.noreply.github.com>
2025-11-28 11:54:58 +0100

    Testing preprocess
    * refactor(preprocess): replacing hard coded file paths with file paths from config
    
    * chore(preprocess): fix incorrect namings
    
    * feat(preprocess): adding __main__ to preprocess file
    
    * fix(preprocess): use RAW_DATA_FILE instead of incorrect TRAINING_DATA_FILE
    
    * fix(preprocess): fixing incorrect axis value
    
    * fix(preprocess): fixing incorrect column name

--- source/preprocess.py
+++ source/preprocess.py
@@ -5,7 +5,8 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-from ..utils import impute_missing_values
+from util import impute_missing_values
+from config import RAW_DATA_FILE, OUTLIER_SUMMARY_FILE, CAT_MISSING_IMPUTE_FILE, SCALER_FILE, COLUMNS_DRIFT_FILE, TRAINING_DATA_FILE, TRAIN_GOLD_FILE
 
 def describe_numeric_col(x: pd.Series) -> pd.Series:
     '''
@@ -19,15 +20,15 @@ def describe_numeric_col(x: pd.Series) -> pd.Series:
         index=["Count", "Missing", "Mean", "Min", "Max"],
     )
 
-def basic_cleaning(df: pd.Dataframe) -> pd.Dataframe:
+def basic_cleaning(df: pd.DataFrame) -> pd.DataFrame:
     '''
     Takes a pandas Dataframe and drop columns.
     Then performs string cleaning
     '''
     df = df.copy()
     #dropping columns
-    to_drop = ["is_active", "marketing_consent", "first_booking", "existing_customer", "last_seen", "domain", "country", "visited_learn_before_booking", "visited_faq"]
-    df = df.drop(columns = [c for c in to_drop])
+    to_drop = ["is_active", "marketing_consent", "first_booking", "existing_customer", "last_seen", "domain", "country", "visited_learn_more_before_booking", "visited_faq"]
+    df = df.drop(columns = [c for c in to_drop], axis = 1)
 
     #do string cleaning
     for c in ["lead_indicator", "lead_id", "customer_code"]:
@@ -50,21 +51,21 @@ def split_cat_cont(df: pd.DataFrame):
 
     return cat_vars, cont_vars
 
-def handle_outliers(cont_vars: pd.Dataframe) -> pd.DataFrame:
+def handle_outliers(cont_vars: pd.DataFrame) -> pd.DataFrame:
     '''
     Handles outliers for cont vars and prints summary to csv
     '''
     cont_vars_cleaned = cont_vars.apply(lambda x: x.clip(lower = (x.mean()-2*x.std()), upper = (x.mean()+2*x.std())))
     outlier_summary = cont_vars_cleaned.apply(describe_numeric_col).T
-    outlier_summary.to_csv('./artifacts/outlier_summary.csv')
+    outlier_summary.to_csv(OUTLIER_SUMMARY_FILE)
     return cont_vars_cleaned
 
-def impute(cat_vars: pd.DataFrame, cont_vars: pd.Dataframe):
+def impute(cat_vars: pd.DataFrame, cont_vars: pd.DataFrame):
     '''
     impute missing values using utils function
     '''
     cat_missing_impute = cat_vars.mode(numeric_only=False, dropna=True)
-    cat_missing_impute.to_csv("./artifacts/cat_missing_impute.csv")
+    cat_missing_impute.to_csv(CAT_MISSING_IMPUTE_FILE)
 
     # Continuous variables missing values
     cont_vars = cont_vars.apply(impute_missing_values)
@@ -76,19 +77,19 @@ def impute(cat_vars: pd.DataFrame, cont_vars: pd.Dataframe):
 
     return cat_vars, cont_vars
 
-def scale(cont_vars: pd.DataFrame, scaler_path):
+def scale(cont_vars: pd.DataFrame):
     '''
     scale the cont vars
     '''
     scaler = MinMaxScaler()
     scaler.fit(cont_vars)
 
-    joblib.dump(value=scaler, filename=scaler_path)
+    joblib.dump(value=scaler, filename=SCALER_FILE)
     cont_vars = pd.DataFrame(scaler.transform(cont_vars), columns=cont_vars.columns)
 
     return cont_vars
 
-def combine_and_document_drift(cont_vars: pd.DataFrame, cat_vars: pd.Dataframe) -> pd.Dataframe:
+def combine_and_document_drift(cont_vars: pd.DataFrame, cat_vars: pd.DataFrame) -> pd.DataFrame:
     '''
     combine the cont vars and cat vars then dump into json for data drift comp
     '''
@@ -99,14 +100,14 @@ def combine_and_document_drift(cont_vars: pd.DataFrame, cat_vars: pd.Dataframe) 
 
     #exporting data drift artifact
     data_columns = list(data.columns)
-    with open('./artifacts/columns_drift.json','w+') as f:           
+    with open(COLUMNS_DRIFT_FILE,'w+') as f:           
         json.dump(data_columns,f)
     
-    data.to_csv('./artifacts/training_data.csv', index=False)
+    data.to_csv(TRAINING_DATA_FILE, index=False)
 
     return data
 
-def binning(data: pd.DataFrame) -> pd.Dataframe:
+def binning(data: pd.DataFrame) -> pd.DataFrame:
     '''
     bin the data
     '''
@@ -122,9 +123,7 @@ def binning(data: pd.DataFrame) -> pd.Dataframe:
     data['bin_source'] = data['source'].map(mapping)
     return data
 
-def preprocess_pipeline(df: pd.DataFrame,
-                        scaler_path: str = "./artifacts/minmax_scaler.joblib",
-                        save_gold_path: str = "./artifacts/train_data_gold.csv") -> pd.DataFrame:
+def preprocess_pipeline(df: pd.DataFrame) -> pd.DataFrame:
     '''
     takes raw data and runs all above functions
     returns a csv with the gold data
@@ -139,11 +138,15 @@ def preprocess_pipeline(df: pd.DataFrame,
     #impute missing
     cat_vars_imputed, cont_vars_imputed = impute(cat_vars, cont_vars_cleaned)
     #scale cont
-    cont_vars_scaled = scale(cont_vars_imputed, scaler_path=scaler_path)
+    cont_vars_scaled = scale(cont_vars_imputed)
     #combine it together
     combined = combine_and_document_drift(cont_vars_scaled, cat_vars_imputed)
     #bin it
     final = binning(combined)
     #save gold data
-    final.to_csv(save_gold_path, index=False)
+    final.to_csv(TRAIN_GOLD_FILE, index=False)
     return final
+
+if __name__ == "__main__":
+    df = pd.read_csv(RAW_DATA_FILE)
+    preprocess_pipeline(df)
