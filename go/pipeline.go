98acf142d39d18aac27071b0055be4e21d71b5fe
szonjauley <szul@itu.dk>
2025-11-28 13:33:15 +0100

    feat(go): adding Build function to pipeline.go

--- go/pipeline.go
+++ go/pipeline.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	// Create a shared context
+	ctx := context.Background()
+
+	// Run the stages of the pipeline
+	if err := Build(ctx); err != nil {
+		fmt.Println("Error:", err)
+		panic(err)
+	}
+}
+
+func Build(ctx context.Context) error {
+	// Initialize Dagger client
+	client, err := dagger.Connect(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	python := client.Container().From("python:3.12.2-bookworm").
+		WithDirectory("source", client.Host().Directory("../source")).
+		WithDirectory("data", client.Host().Directory("../data")).
+		WithDirectory("artifacts", client.Host().Directory("../artifacts")).
+		WithExec([]string{"python", "--version"})
+
+	data := python.WithExec([]string{"python", "source/data.py"})
+
+	preprocess := data.WithExec([]string{"python", "source/preprocess.py"})
+
+	train := preprocess.WithExec([]string{"python", "source/train.py"})
+
+	evaluation := train.WithExec([]string{"python", "source/evaluate.py"})
+
+	_, err = evaluation.
+		Directory("artifacts").
+		Export(ctx, "artifacts")
+	if err != nil {
+		return err
+	}
+
+	_, err = evaluation.
+		Directory("mlruns").
+		Export(ctx, "mlruns")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
