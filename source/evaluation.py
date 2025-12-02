63cd8b9be8eb2cb8ba83b6e82f7b20b22b8d90e8
Cornelius Nielsen <cotn@itu.dk>
2025-12-02 12:22:32 +0100

    fix(evaluation): removing old code now

--- source/evaluation.py
+++ source/evaluation.py
@@ -20,12 +20,8 @@ from config import X_TEST_FILE, Y_TEST_FILE, LR_MODEL_FILE, XGBOOST_MODEL_FILE, 
 def load_test_data() -> tuple[pd.DataFrame, pd.Series]:
     """Load X_test and y_test from CSV files defined in config."""
     X_test = pd.read_csv(X_TEST_FILE)
-    if "Unnamed: 0" in X_test.columns:
-        X_test = X_test.drop(columns=["Unnamed: 0"])
     
     y_test_df = pd.read_csv(Y_TEST_FILE)
-    if "Unnamed: 0" in y_test_df.columns:
-        y_test_df = y_test_df.drop(columns=["Unnamed: 0"])
     
     y_test = y_test_df.iloc[:, 0]
 
