1655ac110d81730fe01f3f87abc12d81c4a494e9
pietrorebecchi <pietrorebecchi003@gmail.com>
2025-12-08 10:25:08 +0100

    style(util): apply PEP 8 formatting to util.py

--- source/util.py
+++ source/util.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def impute_missing_values(x: pd.Series, method: str = "mean") -> pd.Series:
     """
     Imputes the mean/median for numeric columns or the mode for other types.
@@ -8,7 +9,7 @@ def impute_missing_values(x: pd.Series, method: str = "mean") -> pd.Series:
         method (str): Values: "mean", "median"
     """
     if (x.dtype == "float64") | (x.dtype == "int64"):
-        x = x.fillna(x.mean()) if method=="mean" else x.fillna(x.median())
+        x = x.fillna(x.mean()) if method == "mean" else x.fillna(x.median())
     else:
         x = x.fillna(x.mode()[0])
     return x
