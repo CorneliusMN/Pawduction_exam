787f1ad6e788be25a75bf3b7596c4fc56668b62c
pietrorebecchi <pietrorebecchi003@gmail.com>
2025-12-08 10:24:17 +0100

    style(tain): apply PEP 8 formatting to train.py

--- source/train.py
+++ source/train.py
@@ -12,17 +12,19 @@ import dvc.api
 import joblib
 
 from config import TRAIN_GOLD_FILE, XGBOOST_MODEL_FILE, LR_MODEL_FILE, X_TEST_FILE, Y_TEST_FILE
-from wrappers import lr_wrapper 
+from wrappers import lr_wrapper
+
 
 def create_dummy_cols(df: pd.DataFrame, col: str) -> pd.DataFrame:
     """
-    Converts a categorical column into dummy/one-hot encoded columns 
+    Converts a categorical column into dummy/one-hot encoded columns
     and drops the original column.
     """
     df_dummies = pd.get_dummies(df[col], prefix=col, drop_first=True)
     new_df = pd.concat([df, df_dummies], axis=1)
     new_df = new_df.drop(col, axis=1)
     return new_df
+
 
 def onehot_encode(data: pd.DataFrame) -> pd.DataFrame:
     """
@@ -44,14 +46,21 @@ def onehot_encode(data: pd.DataFrame) -> pd.DataFrame:
 
     for col in data:
         data[col] = data[col].astype("float64")
-    
+
     return data
 
-def train_model(model_name: str, experiment_id: str, run_name: str, 
-                autolog_type: str, param_dist: dict, 
-                X_train: pd.DataFrame, y_train: pd.DataFrame, model_path: Path):
+
+def train_model(
+        model_name: str,
+        experiment_id: str,
+        run_name: str,
+        autolog_type: str,
+        param_dist: dict,
+        X_train: pd.DataFrame,
+        y_train: pd.DataFrame,
+        model_path: Path):
     """
-    Takes model, random grid search parameters and train data 
+    Takes model, random grid search parameters and train data
     and saves best model while tracking parameters in MLFlow
 
     model_name: XGBRFClassifier / LogisticRegression
@@ -72,27 +81,34 @@ def train_model(model_name: str, experiment_id: str, run_name: str,
     model_class = model_classes[model_name]
 
     with mlflow.start_run(experiment_id=experiment_id, run_name=run_name) as run:
-    
+
         # Enable autologging
         getattr(mlflow, autolog_type).autolog(log_models=False)
-        
+
         model = model_class(random_state=42)
 
         # Hyperparameter search
-        grid = RandomizedSearchCV(model, param_distributions=param_dist, verbose=3, n_iter=10, cv=3)
+        grid = RandomizedSearchCV(
+            model,
+            param_distributions=param_dist,
+            verbose=3,
+            n_iter=10,
+            cv=3)
         grid.fit(X_train, y_train)
-        
+
         best_model = grid.best_estimator_
-        
-        # Save the model locally and log as pyfunc model for inference 
+
+        # Save the model locally and log as pyfunc model for inference
         if model_name == "LogisticRegression":
-            mlflow.pyfunc.log_model("model", python_model=lr_wrapper(best_model))
+            mlflow.pyfunc.log_model(
+                "model", python_model=lr_wrapper(best_model))
             joblib.dump(best_model, model_path)
         else:
             best_model.save_model(str(model_path))
 
         # Log data version
         mlflow.log_param("data_version", "00000")
+
 
 # Load processed data
 data = pd.read_csv(TRAIN_GOLD_FILE)
@@ -111,8 +127,8 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Save test files
-X_test.to_csv(X_TEST_FILE, index = False)
-y_test.to_csv(Y_TEST_FILE, index = False)
+X_test.to_csv(X_TEST_FILE, index=False)
+y_test.to_csv(Y_TEST_FILE, index=False)
 
 # Define MLflow experiment
 current_date = datetime.datetime.now().strftime("%Y_%B_%d")
@@ -123,22 +139,38 @@ experiment_id = experiment.experiment_id
 
 # Define model parameters
 param_dist_xgb = {
-        "learning_rate": uniform(1e-2, 3e-1),
-        "min_split_loss": uniform(0, 10),
-        "max_depth": randint(3, 10),
-        "subsample": uniform(0, 1),
-        "objective": ["reg:squarederror", "binary:logistic", "reg:logistic"],
-        "eval_metric": ["aucpr", "error"]
-    }
+    "learning_rate": uniform(1e-2, 3e-1),
+    "min_split_loss": uniform(0, 10),
+    "max_depth": randint(3, 10),
+    "subsample": uniform(0, 1),
+    "objective": ["reg:squarederror", "binary:logistic", "reg:logistic"],
+    "eval_metric": ["aucpr", "error"]
+}
 
 param_dist_lr = {
-        "solver": ["newton-cg", "lbfgs", "liblinear", "sag", "saga"],
-        "penalty": ["none", "l1", "l2", "elasticnet"],
-        "C": [100, 10, 1.0, 0.1, 0.01]
-    }
+    "solver": ["newton-cg", "lbfgs", "liblinear", "sag", "saga"],
+    "penalty": ["none", "l1", "l2", "elasticnet"],
+    "C": [100, 10, 1.0, 0.1, 0.01]
+}
 
 # Train XGBoost model
-train_model("XGBRFClassifier", experiment_id, "xgboost_rf", "xgboost", param_dist_xgb, X_train, y_train, XGBOOST_MODEL_FILE)
+train_model(
+    "XGBRFClassifier",
+    experiment_id,
+    "xgboost_rf",
+    "xgboost",
+    param_dist_xgb,
+    X_train,
+    y_train,
+    XGBOOST_MODEL_FILE)
 
 # Train LogisticRegression model
-train_model("LogisticRegression", experiment_id, "logistic_regression", "sklearn", param_dist_lr, X_train, y_train, LR_MODEL_FILE)
+train_model(
+    "LogisticRegression",
+    experiment_id,
+    "logistic_regression",
+    "sklearn",
+    param_dist_lr,
+    X_train,
+    y_train,
+    LR_MODEL_FILE)
